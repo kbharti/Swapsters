8d9bae1a79bcd32a544034d3e3e19eae759635c3
Kamal <kamal.bharti13@gmail.com>
2018-09-23 17:14:09 -0400

    small fix for image path

--- src/services/thingsData.service.ts
+++ src/services/thingsData.service.ts
@@ -12,7 +12,7 @@ export class thingsDataService{
                 Description: "Easy to wear, comfortable sandals for a perfect summer day. Fashionable and trendy.",
                 Summary: "Worn just 3-4 times. Dont fit me anymore, so want to swap this for something more useful. Size-8. Color- back and grey",
                 id: 'ksjn-hs88820-383023',
-                imgUrl: '../assets/imgs/sandal.jpg'
+                imgUrl: 'assets/imgs/sandal.jpg'
             },
             {
                 name : "backpack",
@@ -20,7 +20,7 @@ export class thingsDataService{
                 Description: "Under Armour Backpack. Can be used as office bag, student bag or as a travle kit for short trips",
                 Summary: "Very spacious for a weekend trip. Can easily carry big laptop. Water-proof. Enough space. Used for about 6 months. Very strong material",
                 id: 'ksjn-hs88820-383024',
-                imgUrl: '../assets/imgs/backpack.jpg'
+                imgUrl: 'assets/imgs/backpack.jpg'
             },
             {
                 name : "Water Filter",
@@ -28,7 +28,7 @@ export class thingsDataService{
                 Description: "Water filter for healthy living.",
                 Summary: "Brita made. In working condition. Regularly cleaned and maintained in good condition. Filter change indicators working as well.",
                 id: 'ksjn-hs88820-383025',
-                imgUrl: '../assets/imgs/brita water filter.jpg'
+                imgUrl: 'assets/imgs/brita water filter.jpg'
             },
             {
                 name : "Coffee Table",
@@ -36,7 +36,7 @@ export class thingsDataService{
                 Description: "Coffee table with glass table top. Rounded top.",
                 Summary: "Elegant finish. Serves right purpose for a coffee meet or as a side table for dining hall. Can be easily transported.",
                 id: 'ksjn-hs88820-383025',
-                imgUrl: '../assets/imgs/center table.jpg'
+                imgUrl: 'assets/imgs/center table.jpg'
             },
             {
                 name : "Revolving Chair",
@@ -44,7 +44,7 @@ export class thingsDataService{
                 Description: "Black chair for multi-purpose use. Revolving seat with strong back support.",
                 Summary: "Add luxury to your room or living area with this chair. revolving and adjustable seat. Wheels in perfect working condition.",
                 id: 'ksjn-hs88820-383025',
-                imgUrl: '../assets/imgs/chair.jpg'
+                imgUrl: 'assets/imgs/chair.jpg'
             },
             {
                 name : "Couch Pillow",
@@ -52,7 +52,7 @@ export class thingsDataService{
                 Description: "Couch cushion for your couch or bed.",
                 Summary: "Pair of cushions with stylish designed covers and free of dust or feathers. Always used inside house with no pets.",
                 id: 'ksjn-hs88820-383025',
-                imgUrl: '../assets/imgs/couch pillow.jpg'
+                imgUrl: 'assets/imgs/couch pillow.jpg'
             },
             {
                 name : "Flower Vase",
@@ -60,7 +60,7 @@ export class thingsDataService{
                 Description: "Flower Vase for your side table. Comes with the artifical flowers as shown in the pic.",
                 Summary: "White colored Vase to add to your interior theme of house. Bought new about 8 months back. Flowers are included with the deal.",
                 id: 'ksjn-hs88820-383025',
-                imgUrl: '../assets/imgs/flowerVase.jpg'
+                imgUrl: 'assets/imgs/flowerVase.jpg'
             },
             {
                 name : "Mopper",
@@ -68,7 +68,7 @@ export class thingsDataService{
                 Description: "Mopper with built-in swiffer wet-jet. Great addition to your cleaning supplies.",
                 Summary: "Easy to use mopper with adjustable height and bendable rug. Reaches tough corners and beneath the furniture very easily.",
                 id: 'ksjn-hs88820-383025',
-                imgUrl: '../assets/imgs/mopper.jpg'
+                imgUrl: 'assets/imgs/mopper.jpg'
             },
             {
                 name : "Night Lamp",
@@ -76,7 +76,7 @@ export class thingsDataService{
                 Description: "Night lamp. Can be used for living room or kids room.",
                 Summary: "White colored standing lamp with bulb. Provides dim lights for night-time sleep time. Light weighted and easy to transport. Hood can be removed for increased light.",
                 id: 'ksjn-hs88820-383025',
-                imgUrl: '../assets/imgs/night lamp.jpg'
+                imgUrl: 'assets/imgs/night lamp.jpg'
             },
             {
                 name : "Planter",
@@ -84,7 +84,7 @@ export class thingsDataService{
                 Description: "Planter for inside use. Can be used for medium sized plants.",
                 Summary: "Solid material to hold water inside. Brown colored pot for plants. Great for inside as well as outside use.",
                 id: 'ksjn-hs88820-383025',
-                imgUrl: '../assets/imgs/planter.jpg'
+                imgUrl: 'assets/imgs/planter.jpg'
             }
         ]
     }
